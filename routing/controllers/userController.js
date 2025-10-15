d193c4b2c51b1072c026c9827a872fe8bc69ced1
Raifa509 <fathimathulraifanp@gmail.com>
2025-10-15 13:53:30 +0530

    14/10/25

--- routing/controllers/userController.js
+++ routing/controllers/userController.js
@@ -1,5 +1,5 @@
 const users = require('../../models/userModel')
-
+const jwt = require('jsonwebtoken')
 //register
 exports.registerController = async (req, res) => {
         console.log("inside Register API");
@@ -12,7 +12,7 @@ exports.registerController = async (req, res) => {
                         res.status(409).json("User Already exist!! Please Login")
 
                 } else {
-                        const newUser=new users({
+                        const newUser = new users({
                                 username,
                                 email,
                                 password
@@ -31,15 +31,24 @@ exports.registerController = async (req, res) => {
 exports.loginController = async (req, res) => {
         console.log("Inside Login API");
         // console.log(req.body);
-        const {email, password } = req.body
+        const { email, password } = req.body
         // console.log(email, password);
         try {
-                const existingUser = await users.findOne({ email ,password})
+                const existingUser = await users.findOne({ email })
                 if (existingUser) {
-                        res.status(200).json({user:existingUser})
+
+                        if (existingUser.password == password) {
+                                //token
+                                const token=jwt.sign({userMail:existingUser.email},process.env.JWTSECRET)
+                                res.status(200).json({ user: existingUser,token })
+
+                        } else {
+                                res.status(401).json("Invalid Credential")
+
+                        }
 
                 } else {
-                        res.status(404).json("Invalid Credential")
+                        res.status(404).json("Account Doesn't exist!!!")
                 }
 
         } catch (err) {
