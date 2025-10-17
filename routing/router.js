754f1b65db5a42f7e111530c650932b010c430f3
Raifa509 <fathimathulraifanp@gmail.com>
2025-10-17 09:02:13 +0530

    16/10/25

--- routing/router.js
+++ routing/router.js
@@ -1,5 +1,8 @@
 const express=require("express")
 const userController=require('./controllers/userController')
+const bookController=require('./controllers/bookController')
+const jwtMiddleware = require("../middlewares/jwtMiddlewares")
+
 const router=express.Router()
 
 //register 
@@ -10,5 +13,8 @@ router.post('/login',userController.loginController)
 
 //googlelogin
 router.post('/google-login',userController.googleLoginController)
+
+//add book
+router.post('/add-book',jwtMiddleware,bookController.addBookController)
 
 module.exports=router
