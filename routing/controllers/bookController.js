e71a6cc8fee9dd6a8ebdec185bbbacb1050c3009
Raifa509 <fathimathulraifanp@gmail.com>
2025-11-09 07:08:47 +0530

    payment url updated

--- routing/controllers/bookController.js
+++ routing/controllers/bookController.js
@@ -178,8 +178,11 @@ exports.makeBookPaymentController = async (req, res) => {
             payment_method_types: ["card"],
             line_items,
             mode: 'payment',
-            success_url: 'http://localhost:5173/payment-success',
-            cancel_url: 'http://localhost:5173/payment-error'
+            
+            // success_url: 'http://localhost:5173/payment-success',
+            // cancel_url: 'http://localhost:5173/payment-error'
+            success_url: 'https://book-store-mern-project-nine.vercel.app/payment-success',
+            cancel_url: 'https://book-store-mern-project-nine.vercel.app/payment-error'
         });
         console.log(session);
         res.status(200).json({ checkoutSessionURL: session.url })
